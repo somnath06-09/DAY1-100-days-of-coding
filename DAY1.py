5ad8f0d71305a0028453421e530a8b3ad397a0ca
somnath06-09 <130608623+somnath06-09@users.noreply.github.com>
2023-12-02 21:38:21 +0530

    Add files via upload

---
+++ DAY1.py
@@ -0,0 +1,12 @@
+'''print('Hello World')
+print('Hello World\nHello World\nHello World')
+print('Hello ' + 'Somnath')
+print('Hello' + ' ' + 'Somnath')'''
+# print('hello ' + input('what is your name '))
+
+# boss chalenge of DAY 1
+print('HEYYYY!! HOW ARE YOU DOIN')
+city_name =input('What is your city name?\n ')
+pet_name =input('What is your pet name?\n ')
+combined_name=city_name  + '  ' + pet_name
+print(combined_name)
